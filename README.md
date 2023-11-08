b336ad32ab4882d0d832c646a55274dc77551563
Sifat Ikram <sifatikram@gmail.com>
2023-11-08 14:43:00 +0600

    client side updated

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-features:
+<!-- features:
 1. There are  Website logo, Website name, Home, All
 Jobs, Applied Jobs, Add A Job, My Jobs, Blogs, and User Profile photo asd button added to the navbar. Here Applied Jobs, Add A Job, My Jobs are private route and if user logged in log in button will be changed to sign out button.
 
@@ -10,4 +10,4 @@ Jobs, Applied Jobs, Add A Job, My Jobs, Blogs, and User Profile photo asd button
 5. Extra feature added and website title will be changed according to page.
 6. Firebase authentication for register, log in and log out is added.
 7. If you reload the protected/private routes (after login), this page will not redirect
-the user to the login page. Instead, it will keep the logged-in user on the protected route
+the user to the login page. Instead, it will keep the logged-in user on the protected route -->
--- src/components/utility/Company.jsx
+++ src/components/utility/Company.jsx
@@ -1,42 +1,42 @@
-import React from 'react';
-import facebook from '../../assets/Facebook.JPG';
-import cockroach from '../../assets/cockroachdb.PNG';
-import joomla from '../../assets/joomla.PNG';
-import discord from '../../assets/discord.png';
-import honey from '../../assets/honey.png';
-import library from '../../assets/library.png';
-import pathao from '../../assets/pathao.jpg';
-import Marquee from "react-fast-marquee";
+// import React from 'react';
+// import facebook from '../../assets/Facebook.JPG';
+// import cockroach from '../../assets/cockroachdb.PNG';
+// import joomla from '../../assets/joomla.PNG';
+// import discord from '../../assets/discord.png';
+// import honey from '../../assets/honey.png';
+// import library from '../../assets/library.png';
+// import pathao from '../../assets/pathao.jpg';
+// import Marquee from "react-fast-marquee";
 
-const Company = () => {
-    return (
-        <div className='space-y-5'>
-            <h1 className='text-5xl font-bold text-left'>Our Sponsors</h1>
-            <Marquee className="rounded-box flex items-center bg-base-200">
-                <div className="m-5">
-                    <img src={facebook} className='h-56' alt="facebook" />
-                </div>
-                <div className="m-5">
-                    <img src={cockroach} className='h-32' alt="cockroach" />
-                </div>
-                <div className="m-5">
-                    <img src={joomla} className='h-32' alt="joomla" />
-                </div>
-                <div className="m-5">
-                    <img src={discord} className='h-32' alt="discord" />
-                </div>
-                <div className="m-5">
-                    <img src={honey} className='h-32' alt="honey" />
-                </div>
-                <div className="m-5">
-                    <img src={library} className='h-32' alt="library" />
-                </div>
-                <div className="m-5">
-                    <img src={pathao} className='h-32' alt="pathao" />
-                </div>
-            </Marquee>
-        </div>
-    );
-};
+// const Company = () => {
+//     return (
+//         <div className='space-y-5'>
+//             <h1 className='text-5xl font-bold text-left'>Our Sponsors</h1>
+//             <Marquee className="rounded-box flex items-center bg-base-200">
+//                 <div className="m-5">
+//                     <img src={facebook} className='h-56' alt="facebook" />
+//                 </div>
+//                 <div className="m-5">
+//                     <img src={cockroach} className='h-32' alt="cockroach" />
+//                 </div>
+//                 <div className="m-5">
+//                     <img src={joomla} className='h-32' alt="joomla" />
+//                 </div>
+//                 <div className="m-5">
+//                     <img src={discord} className='h-32' alt="discord" />
+//                 </div>
+//                 <div className="m-5">
+//                     <img src={honey} className='h-32' alt="honey" />
+//                 </div>
+//                 <div className="m-5">
+//                     <img src={library} className='h-32' alt="library" />
+//                 </div>
+//                 <div className="m-5">
+//                     <img src={pathao} className='h-32' alt="pathao" />
+//                 </div>
+//             </Marquee>
+//         </div>
+//     );
+// };
 
-export default Company;
+// export default Company;
